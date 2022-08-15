274c2d64cd700e88b9e4bbfebfa7e4bb50533ea8
AhmedAtef <90651336+AhmedAtef283@users.noreply.github.com>
2022-08-15 05:26:46 +0200

    Create The Brothers.cpp

---
+++ The Brothers.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    string x  , y , a ,b ; 
+    cin >> x >> y >> a >> b ;
+    if(y==b){cout<< "ARE Brothers" ; }
+    else{cout<<"NOT";}
+
+return 0 ; 
+}
