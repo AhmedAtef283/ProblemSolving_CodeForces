f48aac1cd93b35742d553b6948e002453fba045d
AhmedAtef <90651336+AhmedAtef283@users.noreply.github.com>
2022-08-17 05:07:35 +0200

    Create Next Alphabet.cpp

---
+++ Next Alphabet.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    char x ; 
+    cin >> x ; 
+    int y = x+1 ; 
+    if(x==122) {
+        y = 97 ;
+        cout << char (y) ;
+    }
+    else{
+        cout << char (y) ;
+    }
+    
+    
+    
+return 0 ; 
+}
